d875dbe909ec0f70f94f83deedb31746923c7568
haileyhansard <haileyhansard@gmail.com>
2020-06-04 07:44:00 -0700

    Added focus event, updated doubleclick event

--- js/index.js
+++ js/index.js
@@ -2,21 +2,17 @@
 console.log("Project is up, sanity test")
 
 //  * [x] `mouseover`
-// 	* [ ] `keydown`
-// 	* [ ] `wheel`
-// 	* [ ] `drag / drop`
+// 	* [x] `keydown`
+// 	* [x] `wheel`
+// 	* [x] `drag / drop`
 // 	* [ ] `load`
-// 	* [ ] `focus`
+// 	* [x] `focus`
 // 	* [ ] `resize`
 // 	* [ ] `scroll`
 // 	* [ ] `select`
-// 	* [ ] `dblclick`
+// 	* [x] `dblclick`
 
 //mouseover
-// let test = document.getElementById("test");
-// test.addEventListener("mouseover", function(event){
-//     event.target.style.color = "purple";
-// }
 
 let mousePink = document.querySelector("nav");
 mousePink.addEventListener("mouseover", function(event){
@@ -26,29 +22,65 @@ mousePink.addEventListener("mouseover", function(event){
 
 //keydown
 
-// let offWeGo = document.querySelector("intro h2");
-// offWeGo.addEventListener("keydown", function(event){
-// log.textContent += `${e.code}`;
-// })
-
-//offWeGo.addEventListener
-
-//wheel
-
-function zoom(event){
-    event.preventDefault();
-
-    scale += event.deltaY * -0.01;
-
-    scale = Math.min(Math.max(.125, scale), 4);
-
-    el.style.transform = `scale(${scale})`;
-}
-let scale = 1;
-const el = document.querySelector("intro");
-el.onwheel = zoom;
+let offWeGo = document.querySelector(".intro h2");
+document.addEventListener("keydown", function(event){
+console.log (`${event.code}`);
+})
+console.log(offWeGo);
 
 //drag/drop
+const drag = document.querySelector(".intro > p")
+drag.draggable = "true";
+drag.addEventListener("dragstart", e =>{
+    e.target.style.color = "green";
+});
+
+drag.addEventListener("dragend", e => {
+    e.target.style.color = "red";
+});
+
+//wheel
+let btnWheel = document.querySelector(".btn");
+console.log(btnWheel);
+
+btnWheel.addEventListener("wheel", function(){
+    console.log("I was wheeled over!")});
+
+
+//dblclick
+let btnDoubleClick = document.querySelector(".btn");
+console.log(btnDoubleClick);
+
+btnDoubleClick.addEventListener("dblclick", function(e){
+    console.log("I was double clicked!")
+    e.target.style.color = "red";
+});
+
+
+//focus
+const input = document.querySelector("input")
+input.addEventListener('focus', event =>{
+    event.target.placeholder = "HELLO";
+});
+
+
+
+
+// function zoom(event){
+//     event.preventDefault();
+
+//     scale += event.deltaY * -0.01;
+
+//     scale = Math.min(Math.max(.125, scale), 4);
+
+//     el.style.transform = `scale(${scale})`;
+// }
+// let scale = 1;
+// const el = document.querySelector("intro");
+// el.onwheel = zoom;
+
+
+
 
 //load
 
@@ -56,14 +88,6 @@ el.onwheel = zoom;
 
 //resize
 
-//blur
-const blurHeading = document.querySelector("text-content")
-blurHeading.addEventListener('focus', (event) => {
-    event.target.style.background = "yellow";
-});
-blurHeading.addEventListener('blur', (event) => {
-    event.target.style.background = " ";
-});
 
 
 
